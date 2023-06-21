c7b1ff91580f27b168006144299aaed7a4ef41f2
rahulp7801 <76501505+rahulp7801@users.noreply.github.com>
2023-06-21 16:49:42 -0700

    test

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-aryan = 'silly goose'
+aryan = 'silly goose LOLOLOL'
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
