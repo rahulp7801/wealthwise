600ced1e52367240199c2cf5f44d15c4de01f56f
Jayana Nanayakkara <jchdnjc@gmail.com>
2023-06-23 16:53:23 -0700

    stock_chart.py

---
+++ app1/src/Stock_Chart.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import yfinance as yf
+
+stock_list = []
+
+nasdaq_stock_list = pd.read_csv('nasdaqlisted.txt', sep='|')
+other_stock_list = pd.read_csv('otherlisted.txt', sep='|')
+
+nasdaq_df = pd.DataFrame(nasdaq_stock_list)
+other_df = pd.DataFrame(other_stock_list)
+
+for index, row in nasdaq_df.iterrows():
+    stock_list.append(row[0])
+for index, row in other_df.iterrows():
+    stock_list.append(row[0])
+
+
+
+
+tickerSymbol = 'SPY'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDF = tickerData.history(period = '1d', start='2023-5-23', end='2023-6-23')
+tickerDF = tickerDF[['Close']]
+plt.figure(figsize=(10, 4))
+plt.title('SPY From 2015 to 2020', fontsize=20)
+plt.xlabel('Time', fontsize=16)
+plt.ylabel('Price ($)', fontsize=16)
+spyPlot = plt.plot(tickerDF.Close)
+
+for year in range(2015,2024):
+    plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
+plt.show()
