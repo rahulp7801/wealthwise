191d543a6bb61ab09cf06e972d605f23035f394c
Jayana Nanayakkara <jchdnjc@gmail.com>
2023-06-23 17:04:04 -0700

    stock_chart.py

--- app1/src/Stock_Chart.py
+++ app1/src/Stock_Chart.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import yfinance as yf
 
 stock_list = []
@@ -18,19 +17,18 @@ for index, row in nasdaq_df.iterrows():
 for index, row in other_df.iterrows():
     stock_list.append(row[0])
 
+def graphStock(tickersymbol):
+    tickerSymbol = tickersymbol
+    tickerData = yf.Ticker(tickerSymbol)
+    tickerDF = tickerData.history(period = '1d', start='2015-5-23', end='2023-6-23')
+    tickerDF = tickerDF[['Close']]
+    plt.figure(figsize=(10, 4))
+    plt.title('SPY From 2015 to 2020', fontsize=20)
+    plt.xlabel('Time', fontsize=16)
+    plt.ylabel('Price ($)', fontsize=16)
+    plt.plot(tickerDF.Close)
 
-
-
-tickerSymbol = 'SPY'
-tickerData = yf.Ticker(tickerSymbol)
-tickerDF = tickerData.history(period = '1d', start='2023-5-23', end='2023-6-23')
-tickerDF = tickerDF[['Close']]
-plt.figure(figsize=(10, 4))
-plt.title('SPY From 2015 to 2020', fontsize=20)
-plt.xlabel('Time', fontsize=16)
-plt.ylabel('Price ($)', fontsize=16)
-spyPlot = plt.plot(tickerDF.Close)
-
-for year in range(2015,2024):
-    plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
-plt.show()
+    for year in range(2015,2024):
+        plt.axvline(pd.to_datetime(str(year)+'-01-01'), color='k', linestyle='--', alpha=0.2)
+    plt.show()
+graphStock('SPY')
