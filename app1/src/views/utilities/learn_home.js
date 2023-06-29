79b9b9dd6413d1a147a444a64767acbdc5f88c7c
Jayana Nanayakkara <jchdnjc@gmail.com>
2023-06-28 21:31:18 -0700

    learn home

--- app1/src/views/utilities/learn_home.js
+++ app1/src/views/utilities/learn_home.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import Greet from 'views/sample-page/greet.js';
 import 'assets/scss/styles.css';
+import Calendar from 'react-calendar';
+import MainCard from 'ui-component/cards/MainCard';
+import { Typography } from '@mui/material';
 
 class Greeter extends Component {
   render() {
     return (
-      <div className="Greet">
+      <MainCard title={<Typography variant="h1" fontWeight="bold" style={{ textAlign: 'center' }}></Typography>}>
         <div className="grid-container">
-            <div className="grid-item wide-item"><Greet /></div>
+            <MainCard className="grid-item wide-item" >
+                 <Calendar />
+                 <Greet />
+
+            </MainCard>
             <div className="grid-item"><Greet /></div>
         </div>
 
-      </div>
+      </MainCard>
     )
   }
 }
