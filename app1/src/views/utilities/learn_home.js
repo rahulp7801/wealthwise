155b33cc60aa0e4efdba2d37479c35fd48d5c49d
Jayana Nanayakkara <jchdnjc@gmail.com>
2023-06-29 09:36:40 -0700

    learn home

--- app1/src/views/utilities/learn_home.js
+++ app1/src/views/utilities/learn_home.js
@@ -4,6 +4,8 @@ import 'assets/scss/styles.css';
 import Calendar from 'react-calendar';
 import MainCard from 'ui-component/cards/MainCard';
 import { Typography } from '@mui/material';
+import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components';
 
 class Greeter extends Component {
   render() {
@@ -11,8 +13,10 @@ class Greeter extends Component {
       <MainCard title={<Typography variant="h1" fontWeight="bold" style={{ textAlign: 'center' }}></Typography>}>
         <div className="grid-container">
             <MainCard className="grid-item wide-item" >
-                 <Calendar />
-                 <Greet />
+                <CalendarContainer>
+                    <Calendar />
+                </CalendarContainer>
+              <Greet />
 
             </MainCard>
             <div className="grid-item"><Greet /></div>
@@ -24,3 +28,18 @@ class Greeter extends Component {
 }
 
 export default Greeter
+
+const CalendarContainer = styled.div`
+  /* ~~~ container styles ~~~ */
+  /* ~~~ navigation styles ~~~ */
+  /* ~~~ label styles ~~~ */
+  /* ~~~ button styles ~~~ */
+  /* ~~~ day grid styles ~~~ */
+  /* ~~~ neighboring month & weekend styles ~~~ */
+  /* ... */
+
+  /* ~~~ active day styles ~~~ */
+  .react-calendar__tile--range {
+      box-shadow: 0 0 6px 2px black;
+  }
+`;
