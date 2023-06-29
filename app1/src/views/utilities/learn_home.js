ef1e88fba635e1134128b970a7ac3ecd7494bb06
AryanNag <nagpal.aryan@gmail.com>
2023-06-28 21:11:01 -0700

    FORMAT FOR STYLES.CSS FIRE EMOJI

--- app1/src/views/utilities/learn_home.js
+++ app1/src/views/utilities/learn_home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Greet from 'views/sample-page/greet.js';
+import 'assets/scss/styles.css';
 
 class Greeter extends Component {
   render() {
     return (
       <div className="Greet">
-        <Greet />
+        <div className="grid-container">
+            <div className="grid-item wide-item"><Greet /></div>
+            <div className="grid-item"><Greet /></div>
+        </div>
+
       </div>
     )
   }
