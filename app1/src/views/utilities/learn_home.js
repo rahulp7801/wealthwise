08719a305712969f59fec41e0636626fb3ff67aa
Jayana Nanayakkara <jchdnjc@gmail.com>
2023-06-29 14:27:17 -0700

    calendar

--- app1/src/views/utilities/learn_home.js
+++ app1/src/views/utilities/learn_home.js
@@ -4,17 +4,29 @@ import 'assets/scss/styles.css';
 import Calendar from 'react-calendar';
 import MainCard from 'ui-component/cards/MainCard';
 import { Typography } from '@mui/material';
-import 'react-calendar/dist/Calendar.css';
+//import 'react-calendar/dist/Calendar.css';
 import styled from 'styled-components';
 
 class Greeter extends Component {
   render() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
     return (
       <MainCard title={<Typography variant="h1" fontWeight="bold" style={{ textAlign: 'center' }}></Typography>}>
         <div className="grid-container">
             <MainCard className="grid-item wide-item" >
-                <CalendarContainer>
-                    <Calendar />
+                <CalendarContainer >
+                    <Calendar
+                        tileClassName={({ date, view }) =>
+                            view === 'month' &&
+                            date.getFullYear() === currentYear &&
+                            date.getMonth() === currentMonth &&
+                            date.getDate() === currentDay
+                              ? 'react-calendar__tile--active'
+                              : null
+                            } />
                 </CalendarContainer>
               <Greet />
 
@@ -31,15 +43,87 @@ export default Greeter
 
 const CalendarContainer = styled.div`
   /* ~~~ container styles ~~~ */
-  /* ~~~ navigation styles ~~~ */
-  /* ~~~ label styles ~~~ */
-  /* ~~~ button styles ~~~ */
-  /* ~~~ day grid styles ~~~ */
-  /* ~~~ neighboring month & weekend styles ~~~ */
-  /* ... */
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 20px;
+  background-color: #616161;
+  padding: 10px;
+  border-radius: 3px;
 
-  /* ~~~ active day styles ~~~ */
-  .react-calendar__tile--range {
-      box-shadow: 0 0 6px 2px black;
+  /* ~~~ navigation styles ~~~ */
+  .react-calendar__navigation {
+    display: flex;
+
+    .react-calendar__navigation__label {
+      font-weight: bold;
+      font-family: 'Montserrat',;
+    }
+
+    .react-calendar__navigation__arrow {
+      flex-grow: 0.333;
+    }
+  }
+
+  /* ~~~ label styles ~~~ */
+  .react-calendar__month-view__weekdays {
+    text-align: center;
+  }
+
+  /* ~~~ button styles ~~~ */
+  button {
+    margin: 10px;
+    background-color: #616161;
+    border: 0;
+    border-radius: 100px;
+    color: white;
+    padding: 5px 5px;
+
+    &:hover {
+      background-color: #9E9E9E;
+    }
+
+    &:active {
+      background-color: #B39DDB;
+    }
+  }
+
+  /* ~~~ day grid styles ~~~ */
+  .react-calendar__month-view__days {
+    display: grid !important;
+    grid-template-columns: 14.2% 14.2% 14.2% 14.2% 14.2% 14.2% 14.2%;
+
+    .react-calendar__tile {
+      max-width: initial !important;
+    }
+
+    .react-calendar__tile--range {
+      box-shadow: 0 0 0px 0px black;
+    }
+  }
+
+  /* ~~~ neighboring month & weekend styles ~~~ */
+  .react-calendar__month-view__days__day--neighboringMonth {
+    opacity: 0.7;
+  }
+  .react-calendar__month-view__days__day--weekend {
+    color: #F5F5F5;
+  }
+
+  /* ~~~ other view styles ~~~ */
+  .react-calendar__tile--active {
+    background-color: #B39DDB; /* Change the background color as desired */
+    color: #F5F5F5; /* Change the text color as desired */
+
+  .react-calendar__year-view__months, .react-calendar__decade-view__years, .react-calendar__century-view__decades {
+    display: grid !important;
+    grid-template-columns: 20% 20% 20% 20% 20%;
+
+    &.react-calendar__year-view__months {
+      grid-template-columns: 33.3% 33.3% 33.3%;
+    }
+
+    .react-calendar__tile {
+      max-width: initial !important;
+    }
   }
 `;
