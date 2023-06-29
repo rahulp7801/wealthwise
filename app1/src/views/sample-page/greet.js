735521d00156810d02626dd539c4600124898258
AryanNag <nagpal.aryan@gmail.com>
2023-06-28 20:21:35 -0700

    simple changes to Home page

--- app1/src/views/sample-page/greet.js
+++ app1/src/views/sample-page/greet.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Typography } from '@mui/material';
 
+import MainCard from 'ui-component/cards/MainCard';
 
-const Greet = () => {
-  return (
-    <div>
-      <h1>
-        Hello Jayana
-      </h1>
-    </div>
-  )
-}
+const Greet = () => (
+<MainCard title="Current Skills">
+    <Typography variant="body2">
+      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
+      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
+      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
+      descent molls anim id est labours.
+    </Typography>
+  </MainCard>
+);
 
 export default Greet
