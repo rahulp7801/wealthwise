07bd17b6f5acbe874f0b6ba4b40ca48988903720
AryanNag <nagpal.aryan@gmail.com>
2023-07-27 13:06:13 -0700

    message

--- app1/src/portbuilderprops/guest.js
+++ app1/src/portbuilderprops/guest.js
@@ -1,6 +1,20 @@
+import 'assets/scss/styles.css';
+import 'assets/scss/style.scss';
+import React from 'react';
+import { TextField } from '@mui/material';
 const LilGuestAction = () => {
+
+
   return (
     <div>
+        <div className="top-active-stocks-container">
+          Chat Bot
+        </div>
+        <div style={{ position: 'absolute', bottom: 0, width: '85%', padding: '70px' }}>
+          <div style={{ maxWidth: '1500px', margin: '0 auto' }}>
+            <TextField fullWidth label="Enter Your Prompt" id="Text" onKeyPress={(e) => {if (e.key === 'Enter') {console.log(e.target.value);}}}/>
+          </div>
+        </div>
     </div>
   );
 };
--- app1/src/testing.py
+++ app1/src/testing.py
@@ -1,6 +1,11 @@
 from bardapi import Bard
-
 token = 'YggJ-puM3oOTP1xvhMbFpYE1jxMMkB45FK8WZxUzTKnL1nt12-s7KDXzHa1NQOo8UeVf5w.'
+
+
 bard = Bard(token=token)
 
-print(bard.get_answer(f"how much is tesla worth"))
+class ChatBard:
+    self.chat_history = []
+    def start(self):
+
+print(bard.get_answer(f""))
