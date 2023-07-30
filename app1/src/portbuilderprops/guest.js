387acfa9809c11e658bd36799c1c5f1cd67a58f9
AryanNag <nagpal.aryan@gmail.com>
2023-07-29 17:52:36 -0700

    Bard API Guest Mode!

--- app1/src/portbuilderprops/guest.js
+++ app1/src/portbuilderprops/guest.js
@@ -1,22 +1,61 @@
-import 'assets/scss/styles.css';
-import 'assets/scss/style.scss';
-import React from 'react';
+// guest.js
+import React, { useState } from 'react';
 import { TextField } from '@mui/material';
-const LilGuestAction = () => {
 
+const Guest = () => {
+  const [answer, setAnswer] = useState('');
+
+  const handleKeyPress = async (e) => {
+    if (e.key === 'Enter') {
+      try {
+        const enteredPrompt = e.target.value;
+        const response = await fetch('http://localhost:5000/api/get_answer', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: enteredPrompt }), // Send user prompt in API request
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+
+        const data = await response.json();
+
+        // Check if the response has an 'answer' field
+        if (Object.prototype.hasOwnProperty.call(data, 'answer')) {
+          setAnswer(data.answer.content); // Render the 'content' of the answer
+        } else {
+          console.error('Invalid response from the backend:', data);
+          // Handle the error or show a message to the user
+        }
+      } catch (error) {
+        console.error('Error occurred during API request:', error);
+        // Handle the error or show a message to the user
+      }
+    }
+  };
 
   return (
     <div>
-        <div className="top-active-stocks-container">
-          Chat Bot
+      <div className="top-active-stocks-container">
+        Chat Bot
+      </div>
+      <div style={{ position: 'absolute', bottom: 0, width: '85%', padding: '70px' }}>
+        <div style={{ maxWidth: '1500px', margin: '0 auto' }}>
+          {/* Display the generated answer */}
+          {answer && <div style={{ marginBottom: '10px' }}>{answer}</div>}
+          <TextField
+            fullWidth
+            label="Enter Your Prompt"
+            id="Text"
+            onKeyPress={handleKeyPress}
+          />
         </div>
-        <div style={{ position: 'absolute', bottom: 0, width: '85%', padding: '70px' }}>
-          <div style={{ maxWidth: '1500px', margin: '0 auto' }}>
-            <TextField fullWidth label="Enter Your Prompt" id="Text" onKeyPress={(e) => {if (e.key === 'Enter') {console.log(e.target.value);}}}/>
-          </div>
-        </div>
+      </div>
     </div>
   );
 };
 
-export default LilGuestAction;
+export default Guest;
--- app1/src/testing.py
+++ app1/src/testing.py
@@ -1,11 +1,26 @@
+# testing.py (Flask backend)
+from flask import Flask, request, jsonify
 from bardapi import Bard
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire app
 token = 'YggJ-puM3oOTP1xvhMbFpYE1jxMMkB45FK8WZxUzTKnL1nt12-s7KDXzHa1NQOo8UeVf5w.'
-
-
 bard = Bard(token=token)
 
-class ChatBard:
-    self.chat_history = []
-    def start(self):
+@app.route('/api/get_answer', methods=['POST'])
+def get_answer():
+    data = request.json
+    user_prompt = data.get('prompt')  # Receive the user input from the frontend
 
-print(bard.get_answer(f""))
+    print(f"Received prompt from frontend: {user_prompt}")
+
+    # Process the user_prompt using the Bard API (or any other logic) to generate an answer
+    answer = bard.get_answer(user_prompt)
+
+    print(f"Generated answer: {answer}")
+
+    return jsonify({'answer': answer})
+
+if __name__ == '__main__':
+    app.run()
